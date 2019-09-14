fd2dea0d12d9a8aacbf39e9a9db0e38af2696476
JustVice <43510837+JustVice@users.noreply.github.com>
2019-09-14 13:17:12 -0600

    201909141316

--- v2/global-assets/js.js
+++ v2/global-assets/js.js
@@ -1,18 +1,21 @@
+const aside_id = "asideID";
+
 function width_listener() {
     var win = window;
     var x = win.innerWidth || docElem.clientWidth || body.clientWidth;
     if (x < 1025) {
-        document.getElementById("aside-id").style.display = "none";
+        document.getElementById(aside_id).style.display = "none";
     } else {
-        document.getElementById("aside-id").style.display = "block";
+        document.getElementById(aside_id).style.display = "block";
     }
 }
 
 function show_hide_aside() {
-    var value = document.getElementById("aside-id").style.display;
+    var value = document.getElementById(aside_id).style.display;
     if (value != "block") {
-        document.getElementById("aside-id").style.display = "block";
+        document.getElementById(aside_id).style.display = "block";
     } else {
-        document.getElementById("aside-id").style.display = "none";
+        document.getElementById(aside_id).style.display = "none";
     }
 }
+
