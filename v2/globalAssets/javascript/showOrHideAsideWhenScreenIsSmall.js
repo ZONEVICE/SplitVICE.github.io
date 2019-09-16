94c6b9486bdcfbe65924e8322cc8aae5672712f1
JustVice <43510837+JustVice@users.noreply.github.com>
2019-09-16 13:59:50 -0600

    qjkwen8123h781y23

--- v2/globalAssets/javascript/showOrHideAsideWhenScreenIsSmall.js
+++ v2/globalAssets/javascript/showOrHideAsideWhenScreenIsSmall.js
@@ -1,3 +1,4 @@
+/* SHOWS OR HIDES THE ASIDE WHEN THE BUTTON ON HEADER IS CLICKED */
 function show_hide_aside() {
     document.getElementById("asideId").classList.toggle("asideShowWhenScreenIsSmallClass");
 }
