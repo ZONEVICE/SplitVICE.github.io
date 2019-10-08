5a9b3885a973155887fc580ac786e32bbfca37a0
JustVice <43510837+JustVice@users.noreply.github.com>
2019-10-08 01:39:30 -0600

    129uee

--- v2/globalAssets/javascript/loadStaticHtmlsWithAjax.js
+++ v2/globalAssets/javascript/loadStaticHtmlsWithAjax.js
@@ -2,6 +2,7 @@
 const headerPath = "/v2/staticHtmls/header.html";
 const footerPath = "/v2/staticHtmls/footer.html";
 const asidePath = "/v2/staticHtmls/aside.html";
+const MESSAGE_TO_ALL_PAGES_PATH = "/v2/staticHtmls/messageToAllPages.html";
 
 /* LOADS THE HEADER WITH AJAX */
 function loadHeaderWithAjax() {
@@ -36,5 +37,17 @@ function loadAsideWithAjax() {
     }
   };
   xhttp.open("GET", asidePath, true);
+  xhttp.send();
+}
+
+/* LOADS A MESSAGE THAT WILL BE DISPLAYED ON ALL PAGES WITH AJAX */
+function loadMessageToAllPages() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("MESSAGE_TO_ALL_PAGES").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", MESSAGE_TO_ALL_PAGES_PATH, true);
   xhttp.send();
 }
