deaddaff34332e7e27cc649024215ef7bf8533dd
JustVice <43510837+JustVice@users.noreply.github.com>
2020-04-14 22:13:11 -0600

    Comit
    bad folder naming fix

--- public/script/load_htmlViews.js
+++ public/script/load_htmlViews.js
@@ -1,8 +1,8 @@
 /* HTML FILES PATHS */
-const HEADER_PATH = "/public/_html/header.html";
-const FOOTER_PATH = "/public/_html/footer.html";
-const ASIDE_PATH = "/public/_html/aside.html";
-const MESSAGE_TO_ALL_PAGES_PATH = "/public/_html/messageToAllPages.html";
+const HEADER_PATH = "/public/html/header.html";
+const FOOTER_PATH = "/public/html/footer.html";
+const ASIDE_PATH = "/public/html/aside.html";
+const MESSAGE_TO_ALL_PAGES_PATH = "/public/html/messageToAllPages.html";
 
 /* LOADS THE HEADER WITH AJAX */
 function load_header() {
