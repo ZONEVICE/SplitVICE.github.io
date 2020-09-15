8af8e6a87aaf28a12eef268e314395a60072d813
JustVice <43510837+JustVice@users.noreply.github.com>
2020-09-14 20:53:08 -0600

    Send a Message section renovated with NodeJS Email Sender

---
+++ send-message/script.js
@@ -0,0 +1,109 @@
+const contactInfo = document.getElementById("contactInfo");
+const yourMessage = document.getElementById("yourMessage");
+const interactiveArea = document.getElementById("interactiveArea");
+
+const op1 = document.getElementById("op1");
+const op2 = document.getElementById("op2");
+const op3 = document.getElementById("op3");
+const op4 = document.getElementById("op4");
+const op5 = document.getElementById("op5");
+const op6 = document.getElementById("op6");
+
+function submitForm_native() {
+
+    let messageType = getMessageType();
+
+    const msg = {
+        "contactInfo": contactInfo.value,
+        "yourMessage": yourMessage.value,
+        "messageType": messageType
+    };
+    if (areThereAMessage(msg)) {
+        sendMessage(msg);
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Invalid message',
+            text: 'You cannot send an empty message.'
+        })
+    }
+}
+
+function sendMessage(msg) {
+    const url = "https://vicemailer.herokuapp.com/sendemail";
+
+    const textMessage =
+        `Message Type: ${msg.messageType} \r\n
+        Contact info: ${msg.contactInfo} \r\n
+        Message body: ${msg.yourMessage} \r\n
+        End of the message. \r\n
+    `;
+
+    const messageStructure = {
+        "subject": "Message from Send a Message - justvice.github.io",
+        "text": "Plaintext content of the Email.",
+        "html": textMessage
+    }
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(messageStructure), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .catch((error) => {
+            console.error('Error:', error)
+            interactiveArea.innerHTML =
+                `
+            <div class="alert alert-danger" role="alert">
+                An internal server error has happened! Sorry about that!.
+            </div>
+            Here's the message you wrote:
+            ${msg.messageType}${msg.contactInfo}${msg.yourMessage}
+            <br><br>
+            Please, try it again with the Wordpress form.<br>
+            If that does not work either, try reaching me on Twitter.
+            `
+            sendMessageForm_wordpress.style.display = "inline";
+        })
+        .then((response) => {
+            console.log(response)
+            interactiveArea.innerHTML =
+                `
+            <div class="alert alert-success" role="alert">
+                Your message has been sent!
+            </div>
+            `
+        });
+}
+
+function getMessageType() {
+    if (op1.checked) {
+        return "Friendly or hate message";
+    }
+    if (op2.checked) {
+        return "Question";
+    }
+    if (op3.checked) {
+        return "Error report";
+    }
+    if (op4.checked) {
+        return "Suggestion";
+    }
+    if (op5.checked) {
+        return "Commission or business enquiries";
+    }
+    if (op6.checked) {
+        return "Other";
+    }
+    return "No defined.";
+}
+
+// Checks if user has written a message.
+function areThereAMessage(msg) {
+    if (msg.yourMessage != "") {
+        return true;
+    }
+    return false;
+}
---
+++ send-message/showHiddeForms.js
@@ -0,0 +1,37 @@
+const sendMessageForm_wordpress = document.getElementById("sendMessageForm_wordpress");
+const sendMessageForm_native = document.getElementById("sendMessageForm_native");
+const loadSpinner = document.getElementById("loadSpinner");
+
+sendMessageForm_wordpress.style.display = "none";
+sendMessageForm_native.style.display = "none";
+
+function main() {
+
+    const url = "https://vicemailer.herokuapp.com/sendemail";
+
+    fetch(url)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (response) {
+            let obj = JSON.parse(response);
+            if(obj.status){
+                if(obj.status == "up"){
+                    console.log("NodeJS Email Sender's service is up.");
+                    sendMessageForm_native.style.display = "inline";
+                    loadSpinner.style.display = "none";
+                }else{
+                    sendMessageForm_wordpress.style.display = "inline";
+                    loadSpinner.style.display = "none";
+                }
+            }else{
+                sendMessageForm_wordpress.style.display = "inline";
+                loadSpinner.style.display = "none";
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+}
+
+main();
