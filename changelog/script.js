bec0068a96c3e79db95d2e42f5d2de18df69e928
JustVice <43510837+JustVice@users.noreply.github.com>
2021-01-05 14:06:50 -0600

    V-AESEcrypt-2 program added

--- changelog/script.js
+++
@@ -1,5 +0,0 @@
-function load_changeLogContent() {
-    const contentDiv_id = "changeLogDivContent_id";
-    const contentULR = `https://gist.githubusercontent.com/JustVice/20af968f5c874cbdf605291145bf9295/raw/changelog.html`;
-    AJAX(contentDiv_id, contentULR);
-}
