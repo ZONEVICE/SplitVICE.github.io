2bb9a1b5797f1a5ed3a98d4d8a9fff447829baa9
JustVice <43510837+JustVice@users.noreply.github.com>
2021-01-06 11:38:44 -0600

    Home page SEO update.
    Favicon updated to high resolution.
    Deleted Git bat payloads.
    SEO updated in HTML new page template.

--- Git - AddCommitPush - Ask title and description.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p title="Enter your commit title:"
-set /p description="Enter your commit description:"
-git add .
-git commit -m "%title%" -m "%description%"
-git push origin master
-@echo off
-set /p id="Commands finished. Press any key to close."
-
--- Git - AddCommitPush.bat
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -m "."
-git push origin master
-@echo off
-set /p id="Commands finished. Press any key to close."
